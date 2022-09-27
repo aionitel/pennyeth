af5e4b3a383028113d83d42566f627d4406b8176
Alex Ionitel <oranggsoda@gmail.com>
2022-09-27 12:14:42 -0400

    better looking Collection on desktop

--- components/collection/Collection.tsx
+++ components/collection/Collection.tsx
@@ -23,13 +23,13 @@ const Collection: React.FC = () => {
   return (
     <div className='flex-row w-screen lg:max-w-screen-lg 2xl:max-w-screen-xl'>
       <div className='flex justify-between text-md lg:text-sm my-4 ml-2 border-t pt-5 border-lightgray'>
-        <h1 className='ml-5 lg:w-40'>Name</h1>
-        <h1 className='mr-12 lg:w-40'>Price</h1>
+        <h1 className='ml-5 lg:w-48 2xl:w-40'>Name</h1>
+        <h1 className='mr-12 lg:mr-0 lg:w-40'>Price</h1>
         <h1 className='hidden lg:block w-40'>24h%</h1>
         <h1 className='hidden lg:block w-40'>Volume(24h)</h1>
         <h1 className='pr-6 hidden lg:block w-40'>Market Cap</h1>
         <h1 className='hidden lg:block w-40'>Market Dominance</h1>
-        <h1 className='hidden lg:block'>Supply</h1>
+        <h1 className='hidden lg:block mr-8'>Supply</h1>
       </div>
       {
         assets.map(item => (
@@ -78,25 +78,25 @@ const CollectionItem: React.FC<AssetProps> = ({ name, ticker, image, price, dail
   return (
     <Link href={`/${ticker.toLowerCase()}`} passHref>
       <div className='flex justify-between lg:justify-start text-md lg:text-sm py-5'>
-        <div className='flex w-44'>
+        <div className='flex w-44 lg:w-32'>
           <img src={image} alt='' style={{ height: logoSize , width: logoSize, marginTop: 5 }} className='mx-3' />
           <div>
             <h1>{name}</h1>
             <h1 className='text-sm opacity-[0.7]'>{ticker}</h1>
           </div>
         </div>
-        <div className='pt-3 ml-20'>
+        <div className='pt-3 ml-20 w-48'>
           <h1>${price_formatter.format(price)}</h1>
         </div>
-        <div>
+        <div className='w-56'>
           {
             dailyChange < 0 ? <div className='hidden lg:flex pt-2'><RiArrowDownSFill className='text-2xl text-red' /><h1 className='text-red hidden lg:flex pt-1'>{price_formatter.format(dailyChange)}%</h1></div> 
             : <div className='hidden lg:flex pt-2'><RiArrowUpSFill className='hidden lg:flex mt-1 text-2xl text-green-400' /><h1 className='text-green-400 hidden lg:flex pt-1'>{price_formatter.format(dailyChange)}%</h1></div>
           }
         </div>
-        <h1 className='hidden pt-3 lg:flex'>{compact_formtatter.format(volume)} {ticker}</h1>
-        <h1 className='hidden pt-3 lg:flex'>${compact_formtatter.format(marketCap)}</h1>
-        <h1 className='hidden pt-3 lg:flex'>{marketDominance.toFixed(2)}%</h1>
+        <h1 className='hidden pt-3 lg:flex w-52'>{compact_formtatter.format(volume)} {ticker}</h1>
+        <h1 className='hidden pt-3 lg:flex w-52'>${compact_formtatter.format(marketCap)}</h1>
+        <h1 className='hidden pt-3 lg:flex w-44'>{marketDominance.toFixed(2)}%</h1>
         <h1 className='hidden pt-3 lg:flex'>{compact_formtatter.format(supply)}</h1>
       </div>
     </Link>
