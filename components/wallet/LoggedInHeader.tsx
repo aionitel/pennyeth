873c713ee048e6e90fc4bae2762c24f29b44a458
Alex Ionitel <oranggsoda@gmail.com>
2022-10-01 19:50:08 -0400

    Transfer button done, now onto modal

--- components/wallet/LoggedInHeader.tsx
+++ components/wallet/LoggedInHeader.tsx
@@ -12,15 +12,17 @@ const LoggedInHeader: React.FC = () => {
   return (
     <motion.div>
       <div className='flex justify-center mr-6'>
-        <h1><p className='text-green-400 text-center'>Wallet Connected</p><p>{user.slice(0, 8)}...{user.slice(10, 18)}</p></h1>
-        <div className='flex pl-5 pt-4 text-2xl decoration-red' data-tip={(<div>this is a tip</div>)}>
+        <h1><p className='text-green-400 text-center text-sm'>Wallet Connected</p><p>{user.slice(0, 8)}...{user.slice(10, 18)}</p></h1>
+        <div className='flex ml-7 mt-2 text-2xl' data-tip={(<div>this is a tip</div>)}>
             <AiFillCloseCircle className='text-rose-500 hover:opacity-[0.9] hover:cursor-pointer' onClick={() => setUser("")} />
         </div>
           <ReactTooltip place='right'>
             <h1>Disconnect wallet from PennyETH.</h1>
           </ReactTooltip>
       </div>
-      <Transfer />
+      <div className='mt-8'>
+        <Transfer />
+      </div>
     </motion.div>
   )
 }
--- components/wallet/Transfer.tsx
+++ components/wallet/Transfer.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Transfer: React.FC = () => {
   return (
-    <div>Transfer</div>
+    <div 
+      className='
+        bg-blue 
+        text-white 
+        text-center 
+        py-5 
+        hover:scale-105 
+        transition-all 
+        rounded-2xl 
+        text-base 
+        hover:cursor-pointer
+        hover:rounded-none 
+        duration-300
+        mr-12
+        ml-6
+        '
+    >
+      <h1>Send / Receive</h1>
+    </div>
   )
 }
 
