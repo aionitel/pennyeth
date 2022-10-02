4d8f139d6468bf0db5d60ba82938a524eb6eee9d
Alex Ionitel <oranggsoda@gmail.com>
2022-10-02 18:12:55 -0400

    showing qr code and eth address in Receive component

--- components/wallet/Receive.tsx
+++ components/wallet/Receive.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip';
 import { useRecoilValue } from 'recoil';
 import { userAtom } from '../../state/atoms';
 
@@ -7,8 +8,13 @@ const Receive: React.FC = () => {
 
   return (
     <div>
-      <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${user}`} width={250} className='py-10' />
-      <h1 className='text-chartGray py-10 border-t border-chartGray'>ETH address: {user}</h1>
+      <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${user}`} width={180} className='pb-10 ml-2' />
+      <div data-tip={(<div>this is a tip</div>)}
+        className='text-chartGray py-10 border-t border-chartGray text-xs'> ETH Address: {user.slice(0, 5)}...{user.slice(10, 15)}
+      </div>
+      <ReactTooltip place='right'>
+        <h1>{user}</h1>
+      </ReactTooltip>
     </div>
   )
 }
