86cb8f6ed9e19eb8024a974de5c7b69d74168160
Alex Ionitel <oranggsoda@gmail.com>
2022-10-04 21:43:01 -0400

    clipboard icon on AddressSummary copying address to clipboard

--- components/explorer/address/AddressSummary.tsx
+++ components/explorer/address/AddressSummary.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaClipboard } from 'react-icons/fa';
 import { Address } from '../../../data/utils/types';
 
 interface AddressSummaryProps {
@@ -7,16 +8,19 @@ interface AddressSummaryProps {
 
 const AddressSummary: React.FC<AddressSummaryProps> = ({ address }) => {
   return (
-    <div className='lg:flex justify-center flex-row'>
-      <div className='border-2 border-almostBlack rounded p-8'>
+    <div className='lg:flex justify-center flex-row my-10'>
+      <div className='border-2 border-almostBlack rounded p-8 mr-20'>
         <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${address.address}`} width={200} />
       </div>
       <div className='text-chartGray'>
-        <div className='flex justify-between'>
+        <div className='flex justify-between border-b pb-2'>
           <h1 className='mr-20'>Address</h1>
-          <div className='ml-20'>
+          <div className='ml-20 flex'>
             <h1 className='hidden lg:block'>{address.address}</h1>
             <h1 className='lg:hidden block'>{address.address.slice(0, 10)}</h1>
+            <FaClipboard className='ml-2 mt-0.5 hover:text-blue hover:cursor-pointer' onClick={() => {
+              navigator.clipboard.writeText(address.address)
+            }} />
           </div>
         </div>
       </div>
